093631f7d0d63dd807d183442a2b3180950feb7f
vTurbine <vtrubiloff@gmail.com>
2019-02-06 20:48:43 +0300

    Added initial project

---
+++ xrRenderPC_VK/xrRender_VK.cpp
@@ -0,0 +1,42 @@
+
+#include "xrRender_VK.h"
+
+/*
+ * TODO:
+ * Current implementation uses a static library provided by LunarG SDK.
+ * It's a good idea to replace it by ptr calls using 3rd party loaders.
+ *
+ * E.g. https://github.com/zeux/volk
+ */
+#pragma comment(lib, "vulkan-1")
+
+extern "C"
+{
+XR_EXPORT void SetupEnv()
+{
+    GEnv.Render         = nullptr;
+    GEnv.RenderFactory  = nullptr;
+    GEnv.DU             = nullptr;
+    GEnv.UIRender       = nullptr;
+#ifdef DEBUG
+    GEnv.DRender        = nullptr;
+#endif
+    // TODO: console init here
+}
+
+XR_EXPORT pcstr GetModeName()
+{
+    return "renderer_vk";
+}
+
+XR_EXPORT bool CheckRendererSupport()
+{
+    const auto apiVersion = vk::enumerateInstanceVersion();
+    Msg( "* Vulkan Instance ver. %d.%d.%d"
+       , VK_VERSION_MAJOR(apiVersion)
+       , VK_VERSION_MINOR(apiVersion)
+       , VK_VERSION_PATCH(apiVersion)
+    );
+    return (apiVersion >= VK_API_VERSION_1_1);
+}
+}
---
+++ xrRenderPC_VK/xrRender_VK.h
@@ -0,0 +1,9 @@
+#ifndef XRRENDER_VK_H_
+#define XRRENDER_VK_H_
+
+#include "xrEngine/stdafx.h"
+
+#define VK_USE_PLATFORM_WIN32_KHR
+#include <vulkan/vulkan.hpp>
+
+#endif // XRRENDER_VK_H_
