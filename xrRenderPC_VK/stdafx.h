a9108b61f33349ea6e4124ae5fbae938679edc43
vTurbine <vtrubiloff@gmail.com>
2019-02-14 19:19:10 +0300

    Update stdafx.h

--- xrRenderPC_VK/stdafx.h
+++ xrRenderPC_VK/stdafx.h
@@ -1,5 +1,5 @@
-#ifndef XRRENDER_VK_H_
-#define XRRENDER_VK_H_
+#ifndef STDAFX_H_
+#define STDAFX_H_
 
 #include "xrCore/stdafx.h"
 #include "xrEngine/stdafx.h"
@@ -9,4 +9,4 @@
 #define VK_USE_PLATFORM_WIN32_KHR
 #include <vulkan/vulkan.hpp>
 
-#endif // XRRENDER_VK_H_
+#endif // STDAFX_H_
