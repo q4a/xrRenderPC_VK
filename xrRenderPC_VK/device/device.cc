1e852f73befeed9c8db28284861ff504e31c0ab0
vTurbine <vtrubiloff@gmail.com>
2019-02-18 09:18:12 +0300

    Device: redundant include removed

--- xrRenderPC_VK/device/device.cc
+++ xrRenderPC_VK/device/device.cc
@@ -1,7 +1,6 @@
 #include <algorithm>
 #include <vector>
 
-#include "common.h"
 #include "device/caps.h"
 #include "device/device.h"
 
--- xrRenderPC_VK/device/swapchain.cc
+++ xrRenderPC_VK/device/swapchain.cc
@@ -1,4 +1,3 @@
-#include "common.h"
 #include "device/device.h"
 
 
