f1f1ae3afe5deb69bd32992eb0bece7a9766569d
vTurbine <vtrubiloff@gmail.com>
2019-03-04 15:08:28 +0300

    Resources: Added pipeline shader resource

---
+++ xrRenderPC_VK/resources/pipeline_shader.cc
@@ -0,0 +1,103 @@
+#include "resources/manager.h"
+
+#include "resources/pipeline_shader.h"
+
+
+/**
+ *
+ */
+template <>
+VertexShaders &
+ResourceManager::GetShaderMap<VertexShader>()
+{
+    return vertex_shaders_;
+}
+
+
+/**
+ *
+ */
+template <>
+FragmentShaders &
+ResourceManager::GetShaderMap<FragmentShader>()
+{
+    return fragment_shaders_;
+}
+
+
+/**
+ *
+ */
+VertexShaderPtr
+ResourceManager::CreateVertexShader
+(const std::string &name
+)
+{
+    std::string shader_name{ name };
+
+    // TODO: check for skinning value
+
+    return CreatePipelineShader<VertexShader>
+                ( shader_name
+                , name
+    );
+}
+
+
+/**
+ *
+ */
+FragmentShaderPtr
+ResourceManager::CreateFragmentShader
+        ( const std::string &name
+        )
+{
+    std::string shader_name{ name };
+
+    // TODO: check for MS value
+
+    return CreatePipelineShader<FragmentShader>
+                ( shader_name
+                , name
+    );
+}
+
+
+/**
+ *
+ */
+template <class T>
+std::shared_ptr<T>
+ResourceManager::CreatePipelineShader
+        ( const std::string &shader_name
+        , const std::string &file_name
+        )
+{
+    auto &shader_map = GetShaderMap<T>();
+
+    // Check if shader already created
+    const auto &iterator = shader_map.find(shader_name);
+    if (iterator != shader_map.cend())
+    {
+        return iterator->second;
+    }
+
+    // Create a shader object
+    auto shader = std::make_shared<PipelineShader>();
+
+    shader->dwFlags |= xr_resource_flagged::RF_REGISTERED;
+    shader->set_name(shader_name.c_str());
+    shader_map.insert(std::make_pair(shader_name, shader));
+
+    // TODO: fix file name: R4 removes '(' and all symbols after
+    shader->file_name   = { shader_name + ShaderTypeTraits<T>::file_extension };
+    shader->entry_point = "main";
+    shader->stage       = ShaderTypeTraits<T>::stage;
+
+    bool result = CompileShader( shader_name
+                               , shader
+    );
+    R_ASSERT(result);
+
+    return shader;
+}
---
+++ xrRenderPC_VK/resources/pipeline_shader.h
@@ -0,0 +1,60 @@
+#ifndef RESOURCES_PIPELINE_SHADER_H_
+#define RESOURCES_PIPELINE_SHADER_H_
+
+#include <vector>
+
+#include "xrCore/xr_resource.h"
+
+enum class ShaderStage
+{
+    Vertex,
+    TesselationControl,
+    TesselationEvaluation,
+    Geometry,
+    Fragment
+};
+
+struct PipelineShader
+    : public xr_resource_named
+{
+    std::string file_name;
+    std::string entry_point;
+    ShaderStage stage;
+
+    std::vector<std::uint32_t> spirv;
+    vk::UniqueShaderModule module;
+    // TODO: constant table
+};
+
+struct VertexShader
+    : public PipelineShader
+{
+    // TODO: vertex input format
+};
+
+struct FragmentShader
+    : public PipelineShader
+{
+    // Nothing special
+};
+
+template <typename T>
+struct ShaderTypeTraits;
+
+
+template <>
+struct ShaderTypeTraits<VertexShader>
+{
+    static const inline std::string file_extension = ".vs";
+    static const ShaderStage stage = ShaderStage::Vertex;
+};
+
+
+template <>
+struct ShaderTypeTraits<FragmentShader>
+{
+    static const inline std::string file_extension = ".ps";
+    static const ShaderStage stage = ShaderStage::Fragment;
+};
+
+#endif // RESOURCES_PIPELINE_SHADER_H_
