edf09f019e8aba9260f887e2171039cb7db53221
vTurbine <vtrubiloff@gmail.com>
2019-03-17 11:30:10 +0300

    Blenders: Set: compleete logic added

--- resources/blender_screen_set.cc
+++ resources/blender_screen_set.cc
@@ -4,6 +4,28 @@
 
 using namespace blenders;
 
+enum BlendMode
+{
+    SET,
+    BLEND,
+    ADD,
+    MULTIPLY,
+    MULTIPLY_2X,
+    ALPHA_ADD,
+    MULTIPLY_2X_BD, // TODO: all names aren't clear yet
+    SET_2R,
+    BLEND_2R,
+    BLEND_4R
+};
+
+
+enum BlendCount
+{
+    VER_2   =  7,
+    VER_4   =  9,
+    VER_5   = 10
+};
+
 
 /**
  *
@@ -32,7 +54,21 @@ ScreenSet::ScreenSet()
 {
     description.class_id = B_SCREEN_SET;
     description.version  = 4;
-    // TBI
+
+    blend_.Count      = VER_4;
+    blend_.IDselected = SET;
+
+    aref_.value =  32;
+    aref_.min   =   0;
+    aref_.max   = 255;
+
+    ztest_.value  = false;
+    zwrite_.value = false;
+
+    lighting_.value = false;
+    fog_.value      = false;
+
+    clamp_.value = true;
 }
 
 
@@ -42,7 +78,7 @@ ScreenSet::ScreenSet()
 void
 ScreenSet::Load
         ( IReader &rstream
-        , u16 version
+        , u16      version
         )
 {
     Blender::Load(rstream, version);
@@ -79,7 +115,7 @@ ScreenSet::Load
         break;
     }
 
-    blend_.Count = 10; // ver5
+    blend_.Count = VER_5;
 }
 
 
@@ -91,6 +127,8 @@ ScreenSet::Save
         ( IWriter &wstream
         )
 {
+    Blender::Save(wstream);
+
     // TBI
 }
 
@@ -107,28 +145,111 @@ ScreenSet::Compile
 
     switch (blend_.IDselected)
     {
-    case 1:
-        compiler.PassBegin("stub_notransform_t", "stub_default");
+    case MULTIPLY_2X_BD:
+        compiler.PassBegin("stub_notransform_t", "stub_default_ma");
+        break;
+    case SET_2R:
+        // no break
+    case BLEND_2R:
+        compiler.PassBegin("stub_notransform_t_m2", "stub_default");
+        break;
+    case BLEND_4R:
+        compiler.PassBegin("stub_notransform_t_m4", "stub_default");
         break;
     default:
+        compiler.PassBegin("stub_notransform_t", "stub_default");
         break;
     };
 
     compiler.PassTexture("s_base", compiler.textures[0]);
     compiler.PassSampler("smp_base");
+    if (clamp_.value)
+    {
+        compiler.SamplerAddressing( "smp_base"
+                                  , vk::SamplerAddressMode::eClampToEdge
+        );
+    }
+
     compiler.PassZtest(ztest_.value, zwrite_.value);
 
     switch (blend_.IDselected)
     {
-    case 1: // BLEND
+    case SET:
+        compiler.PassBlend( false
+                          , vk::BlendFactor::eOne
+                          , vk::BlendFactor::eZero
+                          , false
+                          , 0
+        );
+        break;
+    case BLEND:
         compiler.PassBlend( true
-                          , 0/*D3DBLEND_SRCALPHA*/
-                          , 1/*D3DBLEND_INVSRCALPHA*/
+                          , vk::BlendFactor::eSrcAlpha
+                          , vk::BlendFactor::eOneMinusSrcAlpha
+                          , true
+                          , aref_.value
+        );
+        break;
+    case ADD:
+        compiler.PassBlend( true
+                          , vk::BlendFactor::eOne
+                          , vk::BlendFactor::eOne
+                          , false
+                          , aref_.value
+        );
+        break;
+    case MULTIPLY:
+        compiler.PassBlend( true
+                          , vk::BlendFactor::eDstColor
+                          , vk::BlendFactor::eZero
+                          , false
+                          , aref_.value
+        );
+        break;
+    case MULTIPLY_2X:
+        compiler.PassBlend( true
+                          , vk::BlendFactor::eDstColor
+                          , vk::BlendFactor::eSrcColor
+                          , false
+                          , aref_.value
+        );
+        break;
+    case ALPHA_ADD:
+        compiler.PassBlend( true
+                          , vk::BlendFactor::eSrcAlpha
+                          , vk::BlendFactor::eOne
+                          , true
+                          , aref_.value
+        );
+        break;
+    case MULTIPLY_2X_BD:
+        compiler.PassBlend( true
+                          , vk::BlendFactor::eDstColor
+                          , vk::BlendFactor::eSrcColor
+                          , false
+                          , aref_.value
+        );
+        break;
+    case SET_2R:
+        compiler.PassBlend( true
+                          , vk::BlendFactor::eOne
+                          , vk::BlendFactor::eOne
+                          , true
+                          , 0
+        );
+        break;
+    case BLEND_2R:
+        // no break
+    case BLEND_4R:
+        compiler.PassBlend( true
+                          , vk::BlendFactor::eSrcAlpha
+                          , vk::BlendFactor::eOneMinusSrcAlpha
                           , true
                           , aref_.value
         );
         break;
     default:
+        R_ASSERT(false);
         break;
     }
 
