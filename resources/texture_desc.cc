a8ce8cc6f0139f225670d651237d1fc5c156a766
vTurbine <vtrubiloff@gmail.com>
2019-03-22 22:54:56 +0300

    Resources: Texture description: stubs added

--- resources/texture_desc.cc
+++ resources/texture_desc.cc
@@ -1,21 +1,39 @@
 #include "resources/texture_desc.h"
 
 
-/**
- *
- */
+//-----------------------------------------------------------------------------
 void
 TextureDescription::Load()
+{
+    LoadThm("$game_textures$");
+    LoadThm("$level$");
+}
+
+
+//-----------------------------------------------------------------------------
+void
+TextureDescription::Unload()
 {
     // TBI
 }
 
 
-/**
- *
- */
+//-----------------------------------------------------------------------------
+bool
+TextureDescription::GetDetailTexture
+        ( const std::string     &texture_name
+        , std::string           &detail_name
+        , std::function<void()> &detail_scaler
+        )
+{
+    // TBI
+    return false;
+}
+
 void
-TextureDescription::Unload()
+TextureDescription::LoadThm
+        ( const std::string &mount_point
+        )
 {
     // TBI
 }
--- resources/texture_desc.h
+++ resources/texture_desc.h
@@ -1,6 +1,9 @@
 #ifndef RESOURCES_TEXTURE_DESC_H_
 #define RESOURCES_TEXTURE_DESC_H_
 
+#include <functional>
+#include <string>
+
 class TextureDescription
 {
 public:
@@ -9,6 +12,17 @@ public:
 
     void Load();
     void Unload();
+
+    /*!
+     * \brief   Checks if the texture can be detailed
+     */
+    bool GetDetailTexture( const std::string     &texture_name  ///< [in]  texture name to check
+                         , std::string           &detail_name   ///< [out] detail texture name
+                         , std::function<void()> &detail_scaler ///< [out] scaler function
+                         );
+
+private:
+    void LoadThm(const std::string &mount_point);
 };
 
 #endif // RESOURCES_TEXTURE_DESC_H_
