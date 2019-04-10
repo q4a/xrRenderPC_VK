3334376df7a5bdc030bffa7b5b0a55276eec0d13
vTurbine <vtrubiloff@gmail.com>
2019-04-10 22:28:54 +0300

    Backend: slight refactoring

--- backend/backend.cc
+++ backend/backend.cc
@@ -1,5 +1,11 @@
+/*!
+ * \file    backend.cc
+ * \brief   Render backend implementation
+ */
+
 #include "device/device.h"
 #include "backend/backend.h"
+
 
 constexpr std::size_t kilobyte = 1024;
 constexpr std::size_t operator"" _Kb
@@ -21,9 +27,7 @@ constexpr std::size_t index_count           = triangles_num
 constexpr std::size_t index_cache_size      = index_count * Index::size;
 
 
-/**
- *
- */
+//-----------------------------------------------------------------------------
 BackEnd::BackEnd()
     : vertex_stream(vertex_buffer_size)
     , index_stream(index_buffer_size)
@@ -32,9 +36,7 @@ BackEnd::BackEnd()
 }
 
 
-/**
- *
- */
+//-----------------------------------------------------------------------------
 void
 BackEnd::CreateCommandBuffers()
 {
@@ -55,94 +57,23 @@ BackEnd::CreateCommandBuffers()
 }
 
 
-/**
- *
- */
-void
-BackEnd::CreateRenderPass()
-{
-    // A dummy render pass with one subpass only
-    // (no PP, deferred and other fancy stuff yet)
-
-    const auto color_attachment_description = vk::AttachmentDescription()
-        .setFormat(hw.caps.colorFormat)
-        .setSamples(vk::SampleCountFlagBits::e1) // TODO
-        .setLoadOp( psDeviceFlags.test(rsClearBB)
-                    ? vk::AttachmentLoadOp::eClear
-                    : vk::AttachmentLoadOp::eDontCare
-                  )
-        .setStoreOp(vk::AttachmentStoreOp::eStore)
-        .setStencilLoadOp(vk::AttachmentLoadOp::eDontCare)
-        .setStencilStoreOp(vk::AttachmentStoreOp::eDontCare)
-        .setInitialLayout(vk::ImageLayout::eUndefined)
-        .setFinalLayout(vk::ImageLayout::ePresentSrcKHR);
-
-    const auto color_attachment_reference = vk::AttachmentReference()
-        .setAttachment(0)
-        .setLayout(vk::ImageLayout::eColorAttachmentOptimal);
-
-    const auto subpass = vk::SubpassDescription()
-        .setColorAttachmentCount(1)
-        .setPColorAttachments(&color_attachment_reference)
-        .setPipelineBindPoint(vk::PipelineBindPoint::eGraphics);
-
-    const auto subpass_dependency = vk::SubpassDependency()
-        .setSrcSubpass(VK_SUBPASS_EXTERNAL)
-        .setDstSubpass(0)
-        .setDstStageMask(vk::PipelineStageFlagBits::eColorAttachmentOutput)
-        .setSrcStageMask(vk::PipelineStageFlagBits::eColorAttachmentOutput)
-        .setDstAccessMask( vk::AccessFlagBits::eColorAttachmentRead
-                         | vk::AccessFlagBits::eColorAttachmentWrite
-        );
-
-    const auto renderpass_create_info = vk::RenderPassCreateInfo()
-        .setAttachmentCount(1)
-        .setPAttachments(&color_attachment_description)
-        .setSubpassCount(1)
-        .setPSubpasses(&subpass)
-        .setDependencyCount(1)
-        .setPDependencies(&subpass_dependency);
-
-    render_pass = hw.device->createRenderPassUnique(renderpass_create_info);
-
-    // Attach SC images to render pass
-    for (auto& resource : hw.baseRt)
-    {
-        const auto framebuffer_create_info = vk::FramebufferCreateInfo()
-            .setRenderPass(render_pass.get())
-            .setAttachmentCount(1)
-            .setPAttachments(&resource.imageView)
-            .setWidth(hw.draw_rect.width)
-            .setHeight(hw.draw_rect.height)
-            .setLayers(1); // not stereo
-
-        resource.frameBuffer =
-            hw.device->createFramebuffer(framebuffer_create_info);
-    }
-}
-
-
-/**
- *
- */
+//-----------------------------------------------------------------------------
 void
 BackEnd::OnDeviceCreate()
 {
     CreateCommandBuffers();
 
-    CreateRenderPass();
-
     CreateIndexCache();
 
     // create semaphores
     frame_semaphores.resize(hw.baseRt.size());
-    for (auto& semaphore : frame_semaphores)
+    for (auto &semaphore : frame_semaphores)
     {
         semaphore = hw.device->createSemaphoreUnique({});
     }
 
     render_semaphores.resize(hw.baseRt.size());
-    for (auto& semaphore : render_semaphores)
+    for (auto &semaphore : render_semaphores)
     {
         semaphore = hw.device->createSemaphoreUnique({});
     }
@@ -152,13 +83,11 @@ BackEnd::OnDeviceCreate()
 }
 
 
-/**
- *
- */
+//-----------------------------------------------------------------------------
 void
 BackEnd::DestroyCommandBuffers()
 {
-    for (auto& buffer : draw_cmd_buffers_)
+    for (auto &buffer : draw_cmd_buffers_)
     {
         buffer.reset();
     }
@@ -167,35 +96,15 @@ BackEnd::DestroyCommandBuffers()
 }
 
 
-/**
- *
- */
-void
-BackEnd::DestroyRenderPass()
-{
-    for (auto& resource : hw.baseRt)
-    {
-        //resource.frameBuffer;
-    }
-    render_pass.reset();
-}
-
-
-/**
- *
- */
+//-----------------------------------------------------------------------------
 void
 BackEnd::OnDeviceDestroy()
 {
     DestroyCommandBuffers();
-
-    DestroyRenderPass();
 }
 
 
-/**
- *
- */
+//-----------------------------------------------------------------------------
 void
 BackEnd::CreateIndexCache()
 {
