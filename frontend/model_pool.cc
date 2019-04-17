cb8111468fe4d0dce9a53c1eba9e89d270b6a51b
vTurbine <vtrubiloff@gmail.com>
2019-04-17 22:18:07 +0300

    Frontend: Model pool: basic implementation

--- frontend/model_pool.cc
+++ frontend/model_pool.cc
@@ -3,17 +3,205 @@
  * \brief   Mesh manager implementation
  */
 
+#include "xrCore/FMesh.hpp"
+#include "xrGame/GamePersistent.h"
+
+#include "frontend/models/hierrarhy_visual.h"
+#include "frontend/models/kinematics_animated.h"
+#include "frontend/models/particle_effect.h"
+#include "frontend/models/particle_group.h"
+
 #include "frontend/model_pool.h"
 
 
 //-----------------------------------------------------------------------------
+std::shared_ptr<RenderVisual>
+ModelPool::CreateInstance
+        ( std::uint32_t type
+        )
+{
+    std::shared_ptr<RenderVisual> instance;
+
+    switch (type)
+    {
+    case MT_HIERRARHY:
+        instance = std::make_shared<HierrarhyVisual>();
+        break;
+    case MT_SKELETON_ANIM:
+        instance = std::make_shared<KinematicsAnimated>();
+        break;
+    case MT_SKELETON_RIGID:
+        instance = std::make_shared<Kinematics>();
+        break;
+    case MT_PARTICLE_EFFECT:
+        instance = std::make_shared<ParticleEffect>();
+        break;
+    case MT_PARTICLE_GROUP:
+        instance = std::make_shared<ParticleGroup>();
+        break;
+    default:
+        FATAL("Not implemented");
+    }
+    instance->type_ = type;
+    return instance;
+}
+
+
+//-----------------------------------------------------------------------------
+std::shared_ptr<RenderVisual>
+ModelPool::DuplicateInstance
+        ( const std::shared_ptr<RenderVisual> &source_instance
+        )
+{
+    auto instance = CreateInstance(source_instance->getType());
+    instance->Copy(source_instance.get());
+    instance->Spawn();
+    return instance;
+}
+
+//-----------------------------------------------------------------------------
+void
+ModelPool::RegisterInstance
+        ( const std::string                   &name
+        , const std::shared_ptr<RenderVisual> &instance
+        )
+{
+    ModelDescription description;
+    description.name  = name;
+    description.model = instance;
+    models_.push_back(description);
+}
+
+
+//-----------------------------------------------------------------------------
+std::shared_ptr<RenderVisual>
+ModelPool::LoadInstance
+        ( const std::string &name
+        , bool               need_to_register /* = true */
+        )
+{
+    auto file_name = name + ".ogf";
+
+    string_path path;
+    if (!FS.exist(file_name.c_str()))
+    {
+        if (!FS.exist(path, "$level$", file_name.c_str()))
+        {
+            if (!FS.exist(path, "$game_meshes$", file_name.c_str()))
+            {
+                FATAL_F("Can't find model file '%s'.", file_name.c_str());
+            }
+        }
+    }
+    else
+    {
+        xr_strcpy(path, file_name.c_str());
+    }
+
+    IReader *rstream = FS.r_open(path);
+    ogf_header header;
+    rstream->r_chunk_safe( OGF_HEADER
+                         , &header
+                         , sizeof(header)
+    );
+    FS.r_close(rstream);
+
+    auto instance = CreateInstance(header.type);
+    g_pGamePersistent->RegisterModel(instance.get());
+
+    if (need_to_register)
+    {
+        RegisterInstance(name, instance);
+    }
+
+    return instance;
+}
+
+
+//-----------------------------------------------------------------------------
+std::shared_ptr<RenderVisual>
+ModelPool::FindInstance
+        ( const std::string &name
+        ) const
+{
+    for (const auto &model : models_)
+    {
+        if (model.name == name)
+        {
+            return model.model;
+        }
+    }
+
+    return {};
+}
+
+
+//-----------------------------------------------------------------------------
 RenderVisual *
-ModelPool::Create
+ModelPool::CreateModel
         ( const std::string &name
         , IReader           *rstream
         )
 {
     VERIFY(name.size());
 
+    // TODO: Cast name to lower case and remove extension
+    std::string name_modified = name;
+
+    // Check inactive instances first
+    const auto &iterator = inactive_models_.find(name_modified);
+    if (iterator != inactive_models_.cend())
+    {
+        auto &model = iterator->second;
+        model->Spawn();
+        inactive_models_.erase(iterator);
+
+        return model.get();
+    }
+    else
+    {
+        // Check for active instances
+        auto instance = FindInstance(name_modified);
+        if (!instance)
+        {
+            VERIFY(rstream == nullptr); // Not expect loading from provided stream
+            instance = LoadInstance(name_modified);
+        }
+
+        auto model = DuplicateInstance(instance);
+        registry_.insert(std::make_pair(model, name_modified));
+        return model.get();
+    }
+
     return nullptr;
+}
+
+
+//-----------------------------------------------------------------------------
+RenderVisual *
+ModelPool::CreateParticleEffect
+        ( const std::shared_ptr<ParticleEffectDescriptor> &effect_descriptor
+        )
+{
+    auto instance = CreateInstance(MT_PARTICLE_EFFECT);
+    ParticleEffect *pointer =
+        dynamic_cast<ParticleEffect *>(instance.get());
+
+    pointer->Compile(effect_descriptor);
+    return pointer;
+}
+
+
+//-----------------------------------------------------------------------------
+RenderVisual *
+ModelPool::CreateParticleGroup
+        ( const std::shared_ptr<ParticleGroupDescriptor> &group_descriptor
+        )
+{
+    auto instance = CreateInstance(MT_PARTICLE_GROUP);
+    auto pointer =
+        static_cast<ParticleGroup *>(instance.get());
+
+    pointer->Compile(group_descriptor);
+    return pointer;
 }
--- frontend/model_pool.h
+++ frontend/model_pool.h
@@ -6,10 +6,39 @@
 #ifndef FRONTEND_MODEL_POOL_H_
 #define FRONTEND_MODEL_POOL_H_
 
-#include "frontend/render_visual.h"
+#include <map>
+#include <memory>
+#include <vector>
+
+#include "frontend/models/particle_effect_descriptor.h"
+#include "frontend/models/particle_group_descriptor.h"
+#include "frontend/models/render_visual.h"
+
 
 class ModelPool
 {
+    struct ModelDescription
+    {
+        std::string name;
+        std::shared_ptr<RenderVisual> model;
+    };
+
+    std::shared_ptr<RenderVisual> FindInstance( const std::string &name
+                                              ) const;
+    std::shared_ptr<RenderVisual> LoadInstance( const std::string &name
+                                              , bool               need_to_register = true
+                                              );
+    std::shared_ptr<RenderVisual> DuplicateInstance( const std::shared_ptr<RenderVisual> &instance
+                                                   );
+    void RegisterInstance( const std::string                   &name
+                         , const std::shared_ptr<RenderVisual> &instance
+                         );
+    std::shared_ptr<RenderVisual> CreateInstance( std::uint32_t type
+                                                );
+
+    std::vector<ModelDescription> models_;
+    std::multimap<std::string, std::shared_ptr<RenderVisual>> inactive_models_;
+    std::map<std::shared_ptr<RenderVisual>, std::string> registry_;
 public:
     ModelPool() = default;
     ~ModelPool() = default;
@@ -17,9 +46,11 @@ public:
     /*!
      * \brief   Creates model object
      */
-    RenderVisual *Create( const std::string &name ///< model asset name
-                        , IReader           *rstream ///< stream to read from
-                        );
+    RenderVisual *CreateModel( const std::string &name ///< model asset name
+                             , IReader           *rstream ///< stream to read from
+                             );
+    RenderVisual *CreateParticleEffect(const std::shared_ptr<ParticleEffectDescriptor> &effect_descriptor);
+    RenderVisual *CreateParticleGroup(const std::shared_ptr<ParticleGroupDescriptor> &group_descriptor);
 };
 
 #endif // FRONTEND_MODEL_POOL_H_
